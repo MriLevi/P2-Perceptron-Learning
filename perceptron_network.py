3c910a6bde6539b4b92d46b33275f4fb4634f95c
MriLevi <74299880+MriLevi@users.noreply.github.com>
2022-03-04 21:30:16 +0100

    Updated feed forward and determine_output methods

--- perceptron_network.py
+++ perceptron_network.py
@@ -2,60 +2,53 @@ import math
 
 class Perceptron:
 
-    def __init__(self, threshold, activation_function, weights, target=None, inputs=None):
+    def __init__(self, threshold, activation_function, weights):
         self.bias = -threshold
         self.threshold = threshold
         self.activation_function = activation_function
         self.weights = weights
-        self.target = target
-        self.inputs = inputs
-        self.eta = 0.1
 
     def __str__(self):
         return f'bias: {self.bias} - threshold: {self.threshold} - activation_function: {self.activation_function.__name__}\n' \
-               f'weights: {self.weights} -  inputs: {self.inputs} - output of this perceptron: {self.determine_output()}'
+               f'weights: {self.weights}'
 
     def dot(self, x1, x2):
         """returns the inner product of two vectors"""
         """NOTE: Some checking should be done to verify the dimensions of the input - this is lazy"""
         return sum([x * y for x, y in zip(x1, x2)])
 
-    def determine_output(self):
+    def determine_output(self, inputs):
         """Determines the output of a perceptron by adding the dot product of the weights and inputs and adding bias"""
-        return self.activation_function(self.bias + self.dot(self.weights, self.inputs))
+        return self.activation_function(self.bias + self.dot(self.weights, inputs))
 
-    def update(self):
+    def update(self, inputs, target, learning_rate=0.1):
         """this is pretty much placeholder, haven't got time to make the assignment but want to turn it in anyway
             so I have the right to do the re-do."""
-        y = self.determine_output()
-        error = self.target - y
-        delta_w = self.eta * math.exp * self.weights
-        delta_bias = self.eta * error
-        weights_accent = self.weights + delta_w
-        bias_accent = self.bias + delta_bias
-        pass
+        y = self.determine_output(inputs)
+        error = target - y
+
+        if error > 0:
+            delta_weights = self.dot([learning_rate * error], self.weights)
+            delta_bias = learning_rate * error
+            self.weights = [x+y for x,y in zip(self.weights, delta_weights)]
+            self.bias += delta_bias
+
+
 
     def loss(self):
         """See update"""
         pass
-
-
 
 class PerceptronLayer:
 
     def __init__(self, perceptrons):
         self.perceptrons = perceptrons
 
-    def determine_outputs(self):
+    def determine_outputs(self, inputs):
         """Determines the output for each perceptron by calling determine_output() for each perceptron
            Returns a list of output values."""
-        outputs = [perceptron.determine_output() for perceptron in self.perceptrons]
+        outputs = [perceptron.determine_output(inputs) for perceptron in self.perceptrons]
         return outputs
-
-    def set_inputs(self, inputs):
-        """This function allows us to set the inputs for perceptrons in the layer."""
-        for perceptron in self.perceptrons:
-            perceptron.inputs = inputs
 
 
 class PerceptronNetwork:
@@ -63,14 +56,13 @@ class PerceptronNetwork:
     def __init__(self, perceptron_layers):
         self.perceptron_layers = perceptron_layers
 
-    def feed_forward(self):
+    def feed_forward(self, initial_input):
         """Feed forward determines the output for each layer by using the output of the previous as input for the next
             It determines the first layers outputs outside of the loop to be able to then loop over the remaining
             layers. Finally, it returns the output of the final layer.
             NOTE: The first layer must always have its inputs defined."""
-        inputs = self.perceptron_layers[0].determine_outputs()
+        inputs = self.perceptron_layers[0].determine_outputs(initial_input)
         for perceptron_layer in self.perceptron_layers[1:]:
-            perceptron_layer.set_inputs(inputs)
-            inputs = perceptron_layer.determine_outputs()
+            inputs = perceptron_layer.determine_outputs(inputs)
         result = inputs
-        return result[0] if len(result) == 1 else result
+        return result
